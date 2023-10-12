6db00687dce1d8678affbe58c08f33acd7ad2ca4
Arthur <olehno.arturs@gmail.com>
2023-10-13 01:45:58 +0300

    Initial Commit

---
+++ ConfRide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConfRide
+//
+//  Created by Артур Олехно on 13/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
